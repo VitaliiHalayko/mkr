1c33db55906a29a138399b2ecd31cd039bf8284e
Halaiko Vitalii <galaykovitalii@knu.ua>
2024-04-12 16:50:51 +0300

    creating a start for the program

---
+++ main.py
@@ -0,0 +1,8 @@
+import os
+
+def main(input_file, output_file):
+
+if __name__ == '__main__':
+    input_file = os.path.join('data' 'raw_data.txt')
+    output_file = os.path.join('data' 'result_data.txt')
+    main(input_file, output_file)
