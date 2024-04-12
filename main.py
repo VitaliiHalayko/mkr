3802b5cd95f51a1a4b8623ad726ec378b93c79db
Halaiko Vitalii <galaykovitalii@knu.ua>
2024-04-12 17:06:52 +0300

    analyze data function

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import os
+from collections import defaultdict
 
 def read_file(file_path: str) -> list:
     data_from_file = []
-    with open(file_path, "r") as file:
+    with open(file_path, "r", encoding="utf-8") as file:
         for line in file:
             data = line.strip().split(', ')
             if len(data) == 3:
@@ -10,9 +11,23 @@ def read_file(file_path: str) -> list:
                 data_from_file.append(record)
     return data_from_file
 
+def analyze_data(data_from_file: list, product_name: str):
+    product_data = {}
+    for record in data_from_file:
+        if record["name"] == product_name:
+            product_data.update({record["date"]: record["price"]})
+
+    return product_data
+
+
 def main(input_file, output_file):
+    product_name = input("Enter product name: ")
+
+    data_from_file = read_file(input_file)
+
+    product_data = analyze_data(data_from_file, product_name)
 
 if __name__ == '__main__':
-    input_file = os.path.join('data' 'raw_data.txt')
-    output_file = os.path.join('data' 'result_data.txt')
+    input_file = os.path.join('data/' 'raw_data.txt')
+    output_file = os.path.join('data/' 'result_data.txt')
     main(input_file, output_file)
