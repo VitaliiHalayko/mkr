96f523ac8f7ee134556cc9ca369d68ec66150f3f
Halaiko Vitalii <galaykovitalii@knu.ua>
2024-04-12 16:58:42 +0300

    read file function

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import os
 
+def read_file(file_path: str) -> list:
+    data_from_file = []
+    with open(file_path, "r") as file:
+        for line in file:
+            data = line.strip().split(', ')
+            if len(data) == 3:
+                record = {"name": data[0], "date": data[1], "price": data[2]}
+                data_from_file.append(record)
+    return data_from_file
+
 def main(input_file, output_file):
 
 if __name__ == '__main__':
