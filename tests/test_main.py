96a381c0a8d12e0a4e6366a46adf0efcd501ab37
Halaiko Vitalii <galaykovitalii@knu.ua>
2024-04-12 17:38:01 +0300

    create test save to file function

--- tests/test_main.py
+++ tests/test_main.py
@@ -19,3 +19,12 @@ def test_read_file(input_test_file):
     assert read_file(input_test_file) == [{'name': 'Товар1', 'date': '2024-03-15', 'price': '100.0'},
                                      {'name': 'Товар2', 'date': '2024-03-20', 'price': '150.0'}]
 
+def test_save_to_file(input_test_file, output_file):
+    product_data = {'2024-03-15': '100.0', '2024-03-20': '150.0'}
+    save_to_file(product_data, 'Product1', output_file)
+    with open(output_file, 'r') as file:
+        content = file.read()
+        assert 'Product name: Product1' in content
+        assert '2024-03-15: price: 100.0' in content
+        assert '2024-03-20: price: 150.0 | Price increased by 50' in content
+
