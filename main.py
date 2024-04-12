852db92561b34c4bf7dd9036e8f2eae944070c0d
Halaiko Vitalii <galaykovitalii@knu.ua>
2024-04-12 17:14:48 +0300

    show results function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
-from collections import defaultdict
+import matplotlib.pyplot as plt
+from datetime import datetime
 
 def read_file(file_path: str) -> list:
     data_from_file = []
@@ -11,16 +12,31 @@ def read_file(file_path: str) -> list:
                 data_from_file.append(record)
     return data_from_file
 
-def analyze_data(data_from_file: list, product_name: str):
+
+def analyze_data(data_from_file: list, product_name: str) -> (dict, str):
     product_data = {}
     for record in data_from_file:
         if record["name"] == product_name:
             product_data.update({record["date"]: record["price"]})
 
     if product_data == {}:
-        return "Product not found"
+        return None, "Product not found"
 
-    return product_data
+    return product_data, "Product was found"
+
+
+def show_data(product_data: dict, product_name: str):
+    dates = [datetime.strptime(date, "%Y-%m-%d") for date in product_data.keys()]
+    prices = [float(price) for price in product_data.values()]
+
+    plt.plot(dates, prices, marker='o', linestyle='-')
+    plt.title(f"Price changes for {product_name}")
+    plt.xlabel("Date")
+    plt.ylabel("Price")
+    plt.xticks(rotation=45)
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
 
 
 def main(input_file, output_file):
@@ -28,7 +44,11 @@ def main(input_file, output_file):
 
     data_from_file = read_file(input_file)
 
-    product_data = analyze_data(data_from_file, product_name)
+    product_data, response = analyze_data(data_from_file, product_name)
+
+    show_data(product_data, product_name)
+
+
 
 if __name__ == '__main__':
     input_file = os.path.join('data/' 'raw_data.txt')
