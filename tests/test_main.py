5a9077a39a8199d4ad539c883189b7f41a95c671
Halaiko Vitalii <galaykovitalii@knu.ua>
2024-04-12 17:37:15 +0300

    create test read file function

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,9 +4,18 @@ from datetime import datetime
 from main import read_file, save_to_file, analyze_data
 
 @pytest.fixture
+def input_test_file():
+    return os.path.join('data/' 'test_data.txt')
+
+@pytest.fixture
 def input_file():
     return os.path.join('data/' 'raw_data.txt')
 
 @pytest.fixture
 def output_file():
     return os.path.join('data/' 'result_data.txt')
+
+def test_read_file(input_test_file):
+    assert read_file(input_test_file) == [{'name': 'Товар1', 'date': '2024-03-15', 'price': '100.0'},
+                                     {'name': 'Товар2', 'date': '2024-03-20', 'price': '150.0'}]
+
